9161b1fe0b87666689b9c9e7270e55f7255ea2b8
Varun-Chowdary-V <21bq1a05n9@vvit.net>
2024-12-16 20:30:12 +0530

    SecondCommit

--- Frontend/src/firebaseConfig.js
+++ Frontend/src/firebaseConfig.js
@@ -3,17 +3,17 @@ import { getFirestore } from "firebase/firestore";
 import {getStorage} from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: import.meta.env.VITE_API_KEY || "",
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN || "",
+  projectId: import.meta.env.VITE_PROJECT_ID || "",
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET || "",
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || "",
+  appId: import.meta.env.VITE_APP_ID || "",
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID || "",
 };
 
 // Initialize Firebase
